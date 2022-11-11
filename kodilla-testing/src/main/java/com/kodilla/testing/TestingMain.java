64ddb124466f4c3d0cb14aee46ff92834febf1d2
Witold Zakrzewski <witoldzakrzewski@gmail.com>
2022-11-11 08:34:21 +0100

    6.2 Added Test Case Calculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,34 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        String result = simpleUser.getUsername();
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.add(41, 21);
+        if (sum == 20) {
+            System.out.println("Test kalkulatora zaliczony");
+        } else {
+            System.out.println("Test kalkulatora niezaliczony");
+        }
+
+        int diff = calculator.subtract(41, 21);
+        if (diff == 20) {
+            System.out.println("Test kalkulatora zaliczony");
+        } else {
+            System.out.println("Test kalkulatora niezaliczony");
+        }
     }
 }
