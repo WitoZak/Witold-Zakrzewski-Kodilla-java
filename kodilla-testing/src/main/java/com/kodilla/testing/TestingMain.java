9240bb788de79fa074d41bfc99623d5863260221
Witold Zakrzewski <witoldzakrzewski@gmail.com>
2022-11-12 09:49:13 +0100

    6.3 Added Test Case even List

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,28 +7,5 @@ public class TestingMain {
 
     public static void main(String[] args) {
 
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-        String result = simpleUser.getUsername();
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        int sum = calculator.add(41, 21);
-        if (sum == 20) {
-            System.out.println("Test kalkulatora zaliczony");
-        } else {
-            System.out.println("Test kalkulatora niezaliczony");
-        }
-
-        int diff = calculator.subtract(41, 21);
-        if (diff == 20) {
-            System.out.println("Test kalkulatora zaliczony");
-        } else {
-            System.out.println("Test kalkulatora niezaliczony");
-        }
     }
 }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,57 @@
+package com.kodilla.testing.collection;
+
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
+public class CollectionTestSuite {
+    @BeforeAll
+    static void beforeAll() {
+
+    }
+
+    @BeforeEach
+    public void before() {
+        System.out.println("Test Suite: START");
+    }
+
+    @AfterEach
+    public void after() {
+        System.out.println("Test Suite: END");
+    }
+
+    @DisplayName("Checking if List empty before adding Integers")
+
+    @Test
+    void testOddNumbersExterminatorEmptyList() {
+        //Given
+        List<Integer> emptyList = new ArrayList<>();
+        //When
+        List<Integer> resultList = OddNumbersExterminator.exterminate(emptyList);
+        //Then
+        Assertions.assertEquals(emptyList, resultList);
+    }
+
+    @DisplayName("Exterminating odd numbers and checking if in new List there are only even numbers")
+
+    @Test
+    void testOddNumbersExterminatorNormalList() {
+        //Given
+        List<Integer> normalList = new ArrayList<>();
+        normalList.add(1);
+        normalList.add(2);
+        normalList.add(3);
+        normalList.add(4);
+        normalList.add(5);
+        normalList.add(6);
+        normalList.add(7);
+        //When
+        List<Integer> result = OddNumbersExterminator.exterminate(normalList);
+        List<Integer> expectedOddNumbers = new ArrayList<>();
+        for (int i = 1; i <= normalList.size(); i += 2) {
+            expectedOddNumbers.add(i);
+        }
+        //Then
+        Assertions.assertEquals(expectedOddNumbers, result);
+    }
+}
