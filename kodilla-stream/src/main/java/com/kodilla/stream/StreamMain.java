47a63afaf7a4ead41b19cff06d4f763eb659a34a
Witold Zakrzewski <witoldzakrzewski@gmail.com>
2022-11-16 16:23:30 +0100

    7.1 Added PoemBeautifier

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,33 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.ExpressionExecutor;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+import java.util.*;
+
+public class StreamMain {
+
+    public static void main(String[] args) {
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+
+        PoemBeautifier beautifier = new PoemBeautifier();
+        beautifier.beautify("Hey... How you doin'?", (a) -> a.toUpperCase());
+        beautifier.beautify("Hey... How you doin'?", (a) -> a.toLowerCase());
+        beautifier.beautify("Hey... How you doin'?", (a) -> ";)  " + a + "  ;)");
+        beautifier.beautify("Hey... How you doin'?", (a) -> "<underline>" + a + "</underline>");
+
+    }
+}
