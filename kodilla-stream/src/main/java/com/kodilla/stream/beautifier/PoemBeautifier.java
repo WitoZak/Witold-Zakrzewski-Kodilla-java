7c3bbbbf722a2ea0de24e01b74c79482600642f0
WitoZak <113088417+WitoZak@users.noreply.github.com>
2022-11-17 18:58:31 +0100

    Delete kodilla-stream directory

--- kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
+++
@@ -1,7 +0,0 @@
-package com.kodilla.stream.beautifier;
-
-public class PoemBeautifier {
-    public void beautify(String wordsToBeautify, PoemDecorator poemdecorator) {
-        System.out.println(poemdecorator.decorate(wordsToBeautify));
-    }
-}
